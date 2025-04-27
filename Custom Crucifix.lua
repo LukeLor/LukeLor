f2233f68c44de3cd30c2a57f11f8e5c1ef9f6a9a
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-04-27 15:47:48 -0400

    Update Custom Crucifix.lua

--- Custom Crucifix.lua
+++ Custom Crucifix.lua
@@ -2,4 +2,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local crucifix = LoadCustomInstance("https://github.com/RegularVynixu/Utilities/raw/refs/heads/main/Doors/Item%20Spawner/Assets/Crucifix.rbxm")
 local seal = LoadCustomInstance("")-- Seal link
-local config = {Resist = false, IgnoreEntities = {} }
+local config = {Resist = false, IgnoreEntities = {}, Uses = 1}
+
