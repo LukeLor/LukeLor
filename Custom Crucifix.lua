9cd971f1232ec87508c0d48aad3be97ac5d9c3f0
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-04-27 15:32:54 -0400

    Update and rename Custom Crucifix to Custom Crucifix.lua

---
+++ Custom Crucifix.lua
@@ -0,0 +1,3 @@
+--Currently getting info rn
+local crucifix = LoadCustomInstance("https://github.com/RegularVynixu/Utilities/raw/refs/heads/main/Doors/Item%20Spawner/Assets/Crucifix.rbxm")
+local seal = LoadCustomInstance("")-- Seal link
