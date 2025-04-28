655562de1cbe6cfe42525f62c386b9091059cd94
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-04-28 19:31:07 -0400

    Update Custom Crucifix.lua

--- Custom Crucifix.lua
+++ Custom Crucifix.lua
@@ -1,7 +1,7 @@
 --Currently getting info rn. HEAVILY BASED ON REGULAR VYNIXU'S SCRIPT
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local crucifix = LoadCustomInstance("https://github.com/RegularVynixu/Utilities/raw/refs/heads/main/Doors/Item%20Spawner/Assets/Crucifix.rbxm")
-local seal = LoadCustomInstance("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CrucifixSeal.rbxm")
+local seal = LoadCustomInstance("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/NewerSeal.rbxm")
 local config = {Resist = false, IgnoreEntities = {}, Uses = 1} -- -1 for infinite
 
 local SealIcon = "rbxassetid://123535107502536" --Custom
