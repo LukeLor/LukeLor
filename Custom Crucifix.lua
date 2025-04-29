17e92a8548441856980de16f546cdd0d4c289871
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-04-29 16:49:09 -0400

    Update Custom Crucifix.lua

--- Custom Crucifix.lua
+++ Custom Crucifix.lua
@@ -175,13 +175,13 @@ local pentagram = repentance.CrucSeal
 	end
 
 	-- Crucifix explode
-	TweenService:Create(repentance.Crucifixr, TweenInfo.new(1), { Size = repentance.Crucifix.Size * 3, Transparency = 1 }):Play()
-	TweenService:Create(repentance.CrucSeal.LightAttach.LightBright, TweenInfo.new(1), { Brightness = 0, Range = 0 }):Play()
-	TweenService:Create(repentance.Crucifixr.Light, TweenInfo.new(1), { Brightness = 0, Range = 0 }):Play()
-	TweenService:Create(repentance.CrucSeal.Sigil.SigilReal, TweenInfo.new(2), {Rate = 0 }):Play()
-	TweenService:Create(repentance.CrucSeal.Sigil.SigilBG, TweenInfo.new(2), {Rate = 0 }):Play()
-	TweenService:Create(repentance.CrucSeal.Mark.Assist, TweenInfo.new(2), {Rate = 0 }):Play()
-TweenService:Create(repentance.CrucSeal.Mark.Inner, TweenInfo.new(2), {Rate = 0 }):Play()
+	TweenService:Create(pentagram.Crucifixr, TweenInfo.new(1), { Size = repentance.Crucifix.Size * 3, Transparency = 1 }):Play()
+	TweenService:Create(pentagram.CrucSeal.LightAttach.LightBright, TweenInfo.new(1), { Brightness = 0, Range = 0 }):Play()
+	TweenService:Create(pentagram.Crucifixr.Light, TweenInfo.new(1), { Brightness = 0, Range = 0 }):Play()
+	TweenService:Create(pentagram.CrucSeal.Sigil.SigilReal, TweenInfo.new(2), {Rate = 0 }):Play()
+	TweenService:Create(pentagram.CrucSeal.Sigil.SigilBG, TweenInfo.new(2), {Rate = 0 }):Play()
+	TweenService:Create(pentagram.CrucSeal.Mark.Assist, TweenInfo.new(2), {Rate = 0 }):Play()
+TweenService:Create(pentagram.CrucSeal.Mark.Inner, TweenInfo.new(2), {Rate = 0 }):Play()
 	if not config.Resist then
 		repentance.Crucifix.ExplodeParticle:Emit(math.random(20, 30))
 		moduleScripts.Main_Game.camShaker:ShakeOnce(7.5, 7.5, 0.25, 1.5)
