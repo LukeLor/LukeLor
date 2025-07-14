feb850ed606503ca7d6b8a8d834ca198f5c3d558
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-14 11:29:37 -0400

    Update CaptionDoorsSource.lua

--- CaptionDoorsSource.lua
+++ CaptionDoorsSource.lua
@@ -6,6 +6,10 @@ end
 module.TypeCaption = function(text, typewait) 
 for i = 1, #text do
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(string.sub(text,1,i) ,true)
+    if typewait ~= nil then
 task.wait(typewait)
+    else
+      task.wait(0.2)
+    end
 end
 end
