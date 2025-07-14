e7625e851d89425f170f2ad07ee01a42041ad311
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-14 11:45:52 -0400

    Update CaptionTest.lua

--- CaptionTest.lua
+++ CaptionTest.lua
@@ -1,7 +1,8 @@
-local captionservice = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaptionDoorsSource.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaptionDoorsSource.lua"))()
 
-captionservice.Caption("This is normal.")
+Caption("This is normal.")
 wait(4)
-captionservice.TypeCaption("This is default type")
+TypeCaption("This is default type")
 wait(4)
-captionservice.TypeCaption("This is type at one second", 1)
+TypeCaption("This is type at one second", 1)
+
