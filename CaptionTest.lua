1664bb5b370cf0fd061e472687727faea37a9c5f
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-15 23:08:08 -0400

    Update CaptionTest.lua

--- CaptionTest.lua
+++ CaptionTest.lua
@@ -1,8 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaptionDoorsSource.lua"))()
 
 Caption("This is normal.")
+SpeakerIconShow("123535107502536")
 wait(4)
 TypeCaption("This is default type")
 wait(4)
 TypeCaption("This is type at one second", 1)
+SpeakerIconHide()
+
 
