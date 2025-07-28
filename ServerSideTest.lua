98e39296c18e6b98582ff4dbab6a44cdddc084d3
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-27 21:20:46 -0400

    Update ServerSideTest.lua

--- ServerSideTest.lua
+++ ServerSideTest.lua
@@ -1,5 +1,5 @@
 local event = Instance.new("RemoteEvent")
-
+event:FireServer()
 event.OnServerEvent:Connect(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CharacterModifications.lua"))()
 for _, plrs in pairs(game.Players:GetPlayers()) do
