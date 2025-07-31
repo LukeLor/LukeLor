ebfc5fe3737e716dd3bff05efa3a4deff0eadb1a
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-30 23:37:35 -0400

    Update ServerSideTest.lua

--- ServerSideTest.lua
+++ ServerSideTest.lua
@@ -3,7 +3,7 @@ event:FireServer()
 event.OnServerEvent:Connect(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CharacterModifications.lua"))()
 for _, plrs in pairs(game.Players:GetPlayers()) do
-Vaporize(Color3.fromRGB(0,155,155),Enum.PartType.Ball, plrs)
+Vaporize(Color3.fromRGB(0,225,155),Enum.PartType.Ball, plrs)
       print("Vaporized")
 end
 end)
