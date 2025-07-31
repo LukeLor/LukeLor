b9934a8fcbb438f5d052c71a4c947b9fb1e0f159
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-07-31 10:46:52 -0400

    Update Helpful Rush.lua

--- Helpful Rush.lua
+++ Helpful Rush.lua
@@ -1,7 +1,58 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaptionDoorsSource.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaptionDoorsSource.lua"))()
 
 local char = game.Players.LocalPlayer.CharacterAdded:Wait()
 
+Caption = function(text)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(text,true)
+end
+
+
+TypeCaption = function(text, typewait) 
+for i = 1, #text do
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(string.sub(text,1,i) ,true)
+    if typewait ~= nil then
+task.wait(typewait)
+    else
+      task.wait(0.2)
+    end
+end
+end
+
+SpeakerIconShow = function(IconId)
+   local newui = Instance.new("ScreenGui")
+	newui.Parent = game.Players.LocalPlayer.PlayerGui
+	newui.Name = "SpeakerIcon"
+
+
+	local icon = "rbxassetid://"..IconId
+	local captiontext = game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption
+	local iconholder = Instance.new("ImageLabel")
+	iconholder.Name = "Icon"
+	iconholder.Image = icon
+	iconholder.Parent = newui
+local aspect = Instance.new("UIAspectRatioConstraint")
+aspect.Parent = iconholder
+	local pos =  captiontext.Position + UDim2.new(-.05,-0.031,0,-118)
+	iconholder.Size = UDim2.new(0.1, 0,0.18, 0)
+	iconholder.BackgroundTransparency = 1
+	
+	iconholder.Position = pos
+
+
+	
+
+end
+SpeakerIconHide = function()
+    local newui = game.Players.LocalPlayer.PlayerGui.SpeakerIcon
+	newui.Name = "BeingRemoved"
+    
+local iconholder = newui.Icon
+
+local ts = game:GetService("TweenService")
+    ts:Create(iconholder, TweenInfo.new(.75), {ImageTransparency = 1}):Play()
+
+       game.Debris:AddItem(newui, 1.25)
+end
 
 SolveAnchor = function(item, fully)
 
