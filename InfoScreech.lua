a525e9168bf40243c5e9b530c9bccd4e13f48213
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-08-04 10:31:32 -0400

    Update InfoScreech.lua

--- InfoScreech.lua
+++ InfoScreech.lua
@@ -1,5 +1,5 @@
 local sc = game.ReplicatedStorage.Entities.Screech
-local notwanted = {"Part", "BasePart", "MeshPart", "UnionOperation", "Model"}
+local notwanted = {"Part", "BasePart", "MeshPart", "UnionOperation", "Model", "Motor6D", "ManuelWeld", "Attachment", "PointLight" }
 
 for _, insts in sc:GetDescendants() do
 if insts.Parent ~= nil and not table.find(notwanted, insts.ClassName) then
