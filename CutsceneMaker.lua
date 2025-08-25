05c5f35c8e49821e430ff05246db437c368b72ca
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-08-25 16:52:09 -0400

    Update CutsceneMaker.lua

--- CutsceneMaker.lua
+++ CutsceneMaker.lua
@@ -1,8 +1,37 @@
 local module = {}
 
-module.CamTweenTo = function(destination, time, style, direction)
-game:GetService("TweenService"):Create(workspace.Cam
-end
+module.CamTo = function(destination, time, style, direction)
+  if destination and time and not style and not direction then
+    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time), {CFrame = destination.CFrame}):Play()
+    end
+  end
+  if destination and time and not style and not direction then
+    if destination:IsA("CFrame")  then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time), {CFrame = destination}):Play()
+    end
+  end
+  if destination and time and  style and not direction then
+    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style), {CFrame = destination.CFrame}):Play()
+    end
+  end
+  if destination and time and  style and not direction then
+    if destination:IsA("CFrame")  then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style), {CFrame = destination}):Play()
+    end
+  end
+  if destination and time and  style and  direction then
+    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style,direction), {CFrame = destination.CFrame}):Play()
+    end
+  end
+  if destination and time and  style and  direction then
+    if destination:IsA("CFrame")  then
+game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style, direction), {CFrame = destination}):Play()
+    end
+  end
+    end
 
 
 return module 
