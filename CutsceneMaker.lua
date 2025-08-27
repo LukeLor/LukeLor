706dd4d875c70ae7cf43fc3585e023f33f0a41b1
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2025-08-27 18:36:04 -0400

    Update CutsceneMaker.lua

--- CutsceneMaker.lua
+++ CutsceneMaker.lua
@@ -2,151 +2,138 @@ local module = {}
 
 module.ShakeCamera = function(magnitude, roughness, fadeInTime, fadeOutTime)
 	local camera = workspace.CurrentCamera
-local shaker = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/RbxCameraShaker/refs/heads/master/src/CameraShaker/init.lua"))()
+	local shaker = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/RbxCameraShaker/refs/heads/master/src/CameraShaker/init.lua"))()
 	local camShake = shaker.new(Enum.RenderPriority.Camera.Value, function(shakeCFrame)
-			camera.CFrame =  camera.CFrame * shakeCFrame
-		end)
+		camera.CFrame =  camera.CFrame * shakeCFrame
+	end)
 	if magnitude and roughness and fadeInTime and fadeOutTime then
-	camShake:ShakeOnce(magnitude, roughness, fadeInTime, fadeOutTime)
+		camShake:ShakeOnce(magnitude, roughness, fadeInTime, fadeOutTime)
 	else
 		warn("Missing magnitude, roughness, fadeInTime, or fadeOutTime. ShakeCamera uses: (magnitude, roughness, fadeInTime, fadeOutTime).")
 	end
 end
 
-module.CamTo = function(destination, time, style, direction)
-  if destination ~= nil and time ~= nil then
-  if destination and time and not style and not direction then
-    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time), {CFrame = destination.CFrame}):Play()
-        task.wait(time)
-    end
-  end
-  if destination and time and not style and not direction then
-    if destination:IsA("CFrame")  then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time), {CFrame = destination}):Play()
-    task.wait(time)
-      end
-  end
-  if destination and time and  style and not direction then
-    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style), {CFrame = destination.CFrame}):Play()
-  task.wait(time)
-      end
-  end
-  if destination and time and  style and not direction then
-    if destination:IsA("CFrame")  then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style), {CFrame = destination}):Play()
-  task.wait(time)
-      end
-  end
-  if destination and time and  style and  direction then
-    if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style,direction), {CFrame = destination.CFrame}):Play()
-   task.wait(time)
-      end
-  end
-  if destination and time and  style and  direction then
-    if destination:IsA("CFrame")  then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style, direction), {CFrame = destination}):Play()
-  task.wait(time)
-      end
-  end
-		if destination and time and not style and not direction then
-    if destination:IsA("Attachment") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time), {CFrame = destination.WorldCFrame}):Play()
-        task.wait(time)
-    end
+module.CamTo = function(destination, timeT, style, direction)
+	
+	if destination ~= nil and timeT ~= nil then
+		
+		if destination and timeT and not style and not direction then
+			if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT), {CFrame = destination.CFrame}):Play()
+				task.wait(timeT)
+			end
 		end
-		if destination and time and  style and not direction then
-    if destination:IsA("Attachment") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style), {CFrame = destination.WorldCFrame}):Play()
-        task.wait(time)
-    end
+		if destination and timeT and not style and not direction then
+			if destination:IsA("CFrame")  then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT), {CFrame = destination}):Play()
+				task.wait(timeT)
+			end
 		end
-		if destination and time and  style and  direction then
-    if destination:IsA("Attachment") then
-game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(time, style, direction), {CFrame = destination.WorldCFrame}):Play()
-        task.wait(time)
-    end
+		if destination and timeT and  style and not direction then
+			if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style), {CFrame = destination.CFrame}):Play()
+				task.wait(timeT)
+			end
 		end
-  else
-    warn("Invalid time and or destination. CamTo uses: (destination,time, style, direction). Style and Direction are optional.")
-  end
-    end
+		if destination and timeT and  style and not direction then
+			if destination:IsA("CFrame")  then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style), {CFrame = destination}):Play()
+				task.wait(timeT)
+			end
+		end
+		if destination and timeT and  style and  direction then
+			if destination:IsA("Part") or destination:IsA("BasePart") or destination:IsA("MeshPart") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style,direction), {CFrame = destination.CFrame}):Play()
+				task.wait(timeT)
+			end
+		end
+		if destination and timeT and  style and  direction then
+			if destination:IsA("CFrame")  then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style, direction), {CFrame = destination}):Play()
+				task.wait(timeT)
+			end
+		end
+		if destination and timeT and not style and not direction then
+			if destination:IsA("Attachment") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT), {CFrame = destination.WorldCFrame}):Play()
+				task.wait(timeT)
+			end
+		end
+		if destination and timeT and  style and not direction then
+			if destination:IsA("Attachment") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style), {CFrame = destination.WorldCFrame}):Play()
+				task.wait(timeT)
+			end
+		end
+		if destination and timeT and  style and  direction then
+			if destination:IsA("Attachment") then
+				game:GetService("TweenService"):Create(workspace.CurrentCamera, TweenInfo.new(timeT, style, direction), {CFrame = destination.WorldCFrame}):Play()
+				task.wait(timeT)
+			end
+		end
+	else
+		warn("Invalid time and or destination. CamTo uses: (destination,time, style, direction). Style and Direction are optional.")
+	end
+end
 
 module.CreateScene = function(...)
-	
-local instructions = table.pack(...)
-	
-		--warn("There are no instructions (arguments) to create a scene.")
+
+--return instructions
+	--warn("There are no instructions (arguments) to create a scene.")
 
 end
 
-module.PlayScene = function(Scene, AfterFunction)
-	if Scene then
-	local oldcamtype = workspace.CurrentCamera.CameraType
-	task.wait(0.1)
-	workspace.CurrentCamera.CameraType = Enum.CameraType.Scriptable
-for inum, instruction in Scene do
-Scene[inum]()
-end
-	if AfterFunction then
-AfterFunction()
-	end
-	workspace.CurrentCamera.CameraType = oldcamtype
-	else 
-		warn("No scene was played as no scene was given.")
-	end
-end
+
 
 module.PlayAnimation = function(Rig, Animation, Name)
-  if Rig and Animation then
-if Animation:IsA("String") then
-local animationtrack = Instance.new("Animation")
-    animationtrack.Parent = Rig
-    animationtrack.AnimationId = Animation
-    Animation = animationtrack
-      wait()
-      Animation.Name = Name
-  end
+	if Rig and Animation then
+		if Animation then
+			local animationtrack = Instance.new("Animation")
+			animationtrack.Parent = Rig
+			animationtrack.AnimationId = Animation
+			Animation = animationtrack
+			wait()
+			Animation.Name = Name
+		end
 		Animation.Parent = Rig
-  local player = Rig:WaitForChild("Humanoid") or Rig:WaitForChild("AnimationController")
-  player:LoadAnimation(Animation):Play()
-  else
-    warn("Invalid Rig and or Animation. PlayAnimation uses: (Rig, Animation, Name). Name is optional.")
-  end
+		local player = Rig:WaitForChild("Humanoid") or Rig:WaitForChild("AnimationController")
+		player:LoadAnimation(Animation):Play()
+	else
+		warn("Invalid Rig and or Animation. PlayAnimation uses: (Rig, Animation, Name). Name is optional.")
+	end
+
 end
 
 
 module.PlaySound = function(SoundId, Parent)
-		if SoundId then
-local sound = Instance.new("Sound") 
-    sound.SoundId = SoundId
-    if Parent then
-sound.Parent = Parent
-      else 
-      sound.Parent = workspace
-    end
-    sound:Play()
-		else
-			warn("No SoundId to play..")
+	if SoundId then
+		local sound = Instance.new("Sound") 
+		sound.SoundId = SoundId
+		if Parent then
+			sound.Parent = Parent
+		else 
+			sound.Parent = workspace
 		end
-  end
+		sound:Play()
+	else
+		warn("No SoundId to play..")
+	end
+end
 
-  module.SyncAnimationEvent = function(Animation, SignalName, WhatToDo)
-		if Animation and SignalName then
-Animation:GetMarkerReachedSignal(SignalName):Connect(function(...)
-					if WhatToDo then
-	WhatToDo()
-					else
-						print("Nothing is available.")
-					end
-end)
-				
-		else
-			warn("Invalid Animation and or SignalName. SyncAnimationEvent uses: (Animation, SignalName,WhatToDo). WhatToDo is optional.")
-		end
-  end
+module.SyncAnimationEvent = function(Animation, SignalName, WhatToDo)
+	if Animation and SignalName then
+		Animation:GetMarkerReachedSignal(SignalName):Connect(function(...)
+			if WhatToDo then
+				WhatToDo()
+			else
+				print("Nothing is available.")
+			end
+		end)
 
-  
-    return module 
+	else
+		warn("Invalid Animation and or SignalName. SyncAnimationEvent uses: (Animation, SignalName,WhatToDo). WhatToDo is optional.")
+	end
+end
+
+
+return module 
