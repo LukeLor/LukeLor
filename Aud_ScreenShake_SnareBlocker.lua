c9e831ca53dd7239c6f1e4cb6b8ede16906bd5c5
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-01-21 18:45:00 -0500

    Update Aud_ScreenShake_SnareBlocker.lua

--- Aud_ScreenShake_SnareBlocker.lua
+++ Aud_ScreenShake_SnareBlocker.lua
@@ -15,11 +15,8 @@ function CustomSound(soundLink, vol, sndName)
     return sound
 end
 
-local playerCFrame = workspace.CurrentCamera.CFrame 
-	 local CameraShaker = require(game.ReplicatedStorage.CameraShaker) 
-		local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value, function(shakeCFrame) 	
-			workspace.CurrentCamera.CFrame = workspace.CurrentCamera.CFrame * shakeCFrame 
-		end) 
+local CameraShaker = require(game.Player.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game)
+	
 local locked = CustomSound("https://github.com/LukeLor/LukeLor/blob/main/LockSnareSound.mp3?raw=true",0.5,"SnareLock")
 		locked.Parent = workspace
 locked.TimePosition = 0.15
@@ -27,9 +24,9 @@ locked.TimePosition = 0.15
 		locked.TimePosition = 0.15
 		locked.Volume = 0.5
 		game.TweenService:Create(locked, TweenInfo.new(1.75), {Volume = 0}):Play() 
-		camShake:ShakeOnce(5,15,0,1) 
+		CameraShake.camShaker:ShakeOnce(5,15,0,1) 
 		task.wait(1.375) 
 		locked.TimePosition = 6
 		game.TweenService:Create(locked, TweenInfo.new(1), {Volume = 0.5}):Play()
 
-		camShake:ShakeOnce(5,6,0,1.75)
+		CameraShake.camShaker:ShakeOnce(5,6,0,1.75)
