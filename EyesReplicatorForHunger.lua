5c3a974ba5f4dd8eb43b7f37231c3fafcc191336
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-01-23 15:52:02 -0500

    Update EyesReplicatorForHunger.lua

--- EyesReplicatorForHunger.lua
+++ EyesReplicatorForHunger.lua
@@ -1,5 +1,5 @@
 local currentLoadedRoom = workspace.CurrentRooms[game:GetService("ReplicatedStorage").GameData.LatestRoom.Value]
-local model = game:GetObjects("rbxassetid://11488518082")[1]
-local num = math.floor(#currentLoadedRoom.Nodes:GetChildren() / 2)
+local model = game:GetObjects("rbxassetid://130117055058747")[1]
+local num = math.random(1, #currentLoadedRoom.Nodes:GetChildren())
 model.PrimaryPart.CFrame = (num == 0 and currentLoadedRoom.Base or currentLoadedRoom.Nodes[num]).CFrame + Vector3.new(0, 7, 0)
 model.PrimaryPart.Parent = workspace
