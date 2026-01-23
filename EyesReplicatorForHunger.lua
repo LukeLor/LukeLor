54673953a786885fbe988433377bc30f0d0e3672
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-01-23 17:05:04 -0500

    Update EyesReplicatorForHunger.lua

--- EyesReplicatorForHunger.lua
+++ EyesReplicatorForHunger.lua
@@ -1,5 +1,18 @@
-local currentLoadedRoom = workspace.CurrentRooms[game:GetService("ReplicatedStorage").GameData.LatestRoom.Value]
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LatestRoom = ReplicatedStorage.GameData.LatestRoom.Value
+
+local currentLoadedRoom = workspace.CurrentRooms:FindFirstChild(tostring(LatestRoom-1))
+if not currentLoadedRoom then return end
+
 local model = game:GetObjects("rbxassetid://130117055058747")[1]
-local num = math.random(1, #currentLoadedRoom.Nodes:GetChildren())
-model.PrimaryPart.CFrame = (num == 0 and currentLoadedRoom.Base or currentLoadedRoom.Nodes[num]).CFrame + Vector3.new(0, 7, 0)
-model.PrimaryPart.Parent = workspace
+if not model then return end
+
+model.PrimaryPart = model:FindFirstChildWhichIsA("BasePart")
+if not model.PrimaryPart then return end
+
+local nodes = currentLoadedRoom.PathfindNodes:GetChildren()
+if #nodes == 0 then return end
+
+local node = nodes[math.random(1, #nodes)]
+model:SetPrimaryPartCFrame(node.CFrame + Vector3.new(0, 7, 0))
+model.Parent = workspace
