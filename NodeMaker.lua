f8cad04aa79d445aa8849f19e071e009f8d09134
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-01-27 10:46:22 -0500

    Create NodeMaker.lua

---
+++ NodeMaker.lua
@@ -0,0 +1,24 @@
+function GeneratePath(room)
+local pfs = game:GetService("PathfindingService")
+local startPosition = room:WaitForChild("RoomEntrance").Position
+local goalPosition = room:WaitForChild("RoomExit").Position
+return path:ComputeAsync(startPosition, goalPosition)
+end
+
+function VisualizeNodes(path)
+  local generatednodes= {}
+for wpt in path:GetWaypoints() do
+local part = Instance.new("Part")
+   part.Position = wpt.Position
+   part.Size= Vector3.new(1,1,1)
+  part.Parent = workspace
+    generatednodes[#generatednodes +1] = part
+  end
+  return generatednodes
+end
+
+local nodes = VisualizeNodes(GeneratePath(workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]))
+local cnode = nodes[math.random(1,#nodes)]
+
+local highlight = Instance.new("Highlight")
+highlight.Parent = cnode
