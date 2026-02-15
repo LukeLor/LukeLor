e34aecc5015538c3c54e8fb8088e80f81251a74c
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-02-15 18:54:10 -0500

    Update emote loading and image assignment logic

--- EmoteCollector.lua
+++ EmoteCollector.lua
@@ -5,12 +5,14 @@ local char = workspace.X_masLucas7
 local num = 0
 
 for i, v in pairs(emotes) do
-local emote = game.ReplicatedStorage.Events.LoadAsset:FireServer(v[1])
+local emote = game.InsertService:LoadAsset(v[1])
 local temp = char.Animate.walk:Clone()
+temp.Parent = char.Animate
 temp.Name = string.lower(i)
-  temp:FindFirstChildOfClass("Animation").AnimationId
+temp:FindFirstChildOfClass("Animation").AnimationId = emote:FindFirstChildOfClass("Animation").AnimationId
 num = num+1
-  image = "rbxthumb://0"
+local image = "rbxthumb://type=Asset&id=" ..v[1].. "&w=150&h=150"
 game.Players[char.Name].PlayerGui.MainUi.EmoteWheel.Main.EmoteWheels.Front:FindFirstChildOfClass("TextButton")[tostring(num)].Image = image
+game.Players[char.Name].PlayerGui.MainUi.EmoteWheel.Main.EmoteWheels.Front:FindFirstChildOfClass("TextButton")[tostring(num)]:SetAttribute("Name", i)
   
 end
