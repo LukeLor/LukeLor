100fb204e268f69650ffacc60869f3652a9478f9
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-02-22 19:59:33 -0500

    Update R15ButR6.lua

--- R15ButR6.lua
+++ R15ButR6.lua
@@ -1,7 +1,21 @@
 local char = workspace.X_masLucas7 
 
-local j = char.LeftLowerArm:FindFirstChildOfClass("Motor6D")
-local j2 = char.LeftUpperArm:FindFirstChildOfClass("Motor6D") 
+local j = Instance.new("WeldConstraint")
+local j2 = Instance.new("WeldConstraint")
 
-j.Part0 = char.UpperTorso
-j2.Part0 = j.Parent
+
+j.Part0 = char.LeftLowerArm
+j.Part1 = char.LeftUpperArm
+j.Parent = char
+
+char.LeftUpperArm.Name = "Hi"
+local mainj = nil
+
+for _, j in char.UpperTorso:GetChildren() do
+  if j:IsA("Motor6D") and j.Part1.Name == "LeftUpperArm" then 
+    mainj = j
+    break
+  end
+end
+
+mainj.Part1 = j.Part0
