08f49eab26de12f05ef8aa72912c9f5ab52cbd2c
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-02-22 19:47:50 -0500

    Update R15ButR6.lua

--- R15ButR6.lua
+++ R15ButR6.lua
@@ -1,2 +1,7 @@
 local char = workspace.X_masLucas7 
 
+local j = char.LeftLowerArm:FindFirstChildOfClass("Motor6D")
+local j2 = char.LeftUpperArm:FindFirstChildOfClass("Motor6D") 
+
+j.Part0 = char.UpperTorso
+j2.Part0 = j.Parent
