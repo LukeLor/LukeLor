78de87d246f3c0ca5267d31c97bae94cc13326f0
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-04-10 02:19:29 -0400

    Fix table key syntax in Trumpet_Player.lua

--- Trumpet_Player.lua
+++ Trumpet_Player.lua
@@ -1,12 +1,12 @@
 local keyIndex = {
-  "C" = 115464057786903, 
-  "D" = 102963120140364,
-  "E" = 123672585109549,
-  "F"= 81649589707857,
-  "G"=77562538525120,
-  "A"=76938489240772,
-  "B"=73361471894072,
-  "C2" =107855371255694
+  ["C"] = 115464057786903, 
+  ["D"] = 102963120140364,
+  ["E"] = 123672585109549,
+  ["F"] = 81649589707857,
+  ["G"] =77562538525120,
+  ["A"] =76938489240772,
+  ["B"] =73361471894072,
+  ["C2"] =107855371255694
 }
 
 local MediaPlayer = Instance.new("Sound")
@@ -16,7 +16,7 @@ MediaPlayer.Looped = true
 local Tempo = 120
 
 local Play = function(sequence)
-for individNotes in sequence do 
+for _, individNotes in sequence do 
 local duration = individNotes[2]
 local noteName = individNotes[1]
 local slurred = individNotes[3]
