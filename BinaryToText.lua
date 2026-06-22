219e7c902fd9c9cd49cb7c89d3959a9c305f99d8
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-06-21 20:30:08 -0400

    Update BinaryToText.lua

--- BinaryToText.lua
+++ BinaryToText.lua
@@ -244,4 +244,7 @@ storedtexttolua = usedtable[tostring(string.sub(Text,1, i))]
     return tostring(result)
 end
 end
-    EncodeTo("Challenge Mode should really be in doors, y'know? It's a pretty cool concept that Capztion made! So, LSplash, why don't you, uhhh.... add it?", "binary")
+ local msg=   EncodeTo("Challenge Mode should really be in doors, y'know? It's a pretty cool concept that Capztion made! So, LSplash, why don't you, uhhh.... add it?", "binary")
+print(msg)
+local recode = EncodeTo(msg,"text")
+print(recode)
