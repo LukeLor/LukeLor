3bef7b5a16d353da0dcb7c5b31e068985f8e9aee
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-16 03:34:44 -0400

    Create CoolerDoorsStuff.lua

---
+++ CoolerDoorsStuff.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CoolerVoid.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CoolerScreech.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CoolerGuiding.lua"))()
