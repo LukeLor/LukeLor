834a484e2a4fc1af344851da3b0f469860cbfe9c
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-30 19:58:51 -0400

    Update CaisoShop.lua

--- CaisoShop.lua
+++ CaisoShop.lua
@@ -56,6 +56,12 @@ Shop.PrimaryPart.CanCollide = false
 --Haha, just caption that you cant pick up. (For now.)
 			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this item, due to the fact I don't even have the correct currency...",true)
     end)
+	local gumdropPrompt = Shop.Shop.Gumdrop.Root.Outer.PurchasePrompt
+	gumdropPrompt.Parent.Parent.Anchored = true
+ gumdropPrompt.Triggered:Connect(function()
+--Haha, just caption that you cant pick up. (For now.)
+			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this food, supported by the fact I don't even have the correct currency...",true)
+    end)
 	game.Players.LocalPlayer.Character:PivotTo(Shop.PrimaryPart.CFrame)
 	require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Player Teleported.",true)
 end
