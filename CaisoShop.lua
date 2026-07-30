c0ad975830171c68a2f5c112371d17435a02352f
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-30 02:40:03 -0400

    Update CaisoShop.lua

--- CaisoShop.lua
+++ CaisoShop.lua
@@ -8,13 +8,49 @@ if met then
   local AnimatorStore = game:GetObjects("rbxassetid://104077319394815")[1] --Thanks, Proxiom!!!
 local AnimatorModule = require(AnimatorStore.ReplicatedStorage.Animator)
 
+  --Anim functions
+  local LoadAnim = function(Rig, Animation)
+	local Hum = Rig:FindFirstChildOfClass("Humanoid") 
+	if Hum == nil then
+Hum = Instance.new("Humanoid")
+		if Rig:FindFirstChildOfClass("AnimationController") then
+			local aC_Name = Rig:FindFirstChildOfClass("AnimationController").Name
+Rig:FindFirstChildOfClass("AnimationController"):Destroy()
+			Hum.Parent = Rig 
+			Hum.Name = aC_Name
+    end
+		Hum.Parent = Rig 
+end
+		local Animator = AnimatorModule.new(Hum)
+local AnimationTrack = Animator:LoadAnimation(Animation) 
+	return AnimationTrack
+end
+
+local PlayAnim = function(AnimationTrack)
+AnimationTrack:Play()
+end
+local StopAnim = function(AnimationTrack)
+AnimationTrack:Stop()
+  end
 
 --Shop model
 local Shop = LoadCustomInstance("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/CaisoShop.rbxm")
 
-if Shop then 
-Shop.Parent = workspace
-Shop:PivotTo(roomCFrame)
+if not Shop then 
+--RBXASSETID ALTERNATIVE
   end
   
+  Shop.Parent = workspace
+Shop:PivotTo(roomCFrame)
+Shop.PrimaryPart.Transparency = 1 
+Shop.PrimaryPart.CanCollide = false
+--Yay, Caiso content!
+    local idleK = Shop.Caiso.RawAnimations.Idle
+    local idlePlay = LoadAnim(Shop.Caiso, idleK)
+   PlayAnim(idlePlay)
+  --Test Item Purchase
+  local chainPrompt = Shop.Shop.IsolatedChainPurchase.Body.PurchasePrompt
+ chainPrompt.Triggered:Connect(function()
+--Haha, just caption that you cant pick up.
+    end)
 end
