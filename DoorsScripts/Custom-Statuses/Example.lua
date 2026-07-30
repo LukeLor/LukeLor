76013a4858982a248db559d09261602f69f4ff29
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-30 14:55:24 -0400

    Update Example.lua

--- DoorsScripts/Custom-Statuses/Example.lua
+++ DoorsScripts/Custom-Statuses/Example.lua
@@ -1,4 +1,10 @@
 local StatusMaker = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsScripts/Custom-Statuses/Main.luau"))()
 
-local NoEnergyStatus = StatusMaker:MakeStatus
+local NoEnergyStatus = StatusMaker:MakeStatus("126374173893352", "NoEnergy")
+
+NoEnergyStatus.Visible = true
+
+task.wait(5)
+
+NoEnergyStatus:Destroy()
 
