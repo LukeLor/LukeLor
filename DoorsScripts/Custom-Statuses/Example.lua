f5a6cdfc17e6a5341372416e23b7cb852f561a10
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-30 14:53:07 -0400

    Add Example.lua for custom status creation

---
+++ DoorsScripts/Custom-Statuses/Example.lua
@@ -0,0 +1,4 @@
+local StatusMaker = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsScripts/Custom-Statuses/Main.luau"))()
+
+local NoEnergyStatus = StatusMaker:MakeStatus
+
