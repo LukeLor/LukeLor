ac88f4ed690be3563372ea89aa92c277c2de4748
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-07-31 20:17:50 -0400

    Update Paths.lua

--- Paths.lua
+++ Paths.lua
@@ -1,5 +1,7 @@
 -- TUE VFX: TUE.Root.Attachment. //Attack, Close, Open, SparkOpen, SparkClosed
 
+--Entity Finder: [Item].Screen.ScreenUI.Frame.Frame. // Entity, Warning, StudLocation (OR).Battery // Battery,Battery1, Battery2, Battery3
+
 --Shop interact: CaisoShop.Shop.IsolatedChainPurchase.Body.PurchasePrompt
 
 
