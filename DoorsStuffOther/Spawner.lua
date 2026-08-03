f3610cadba3e9ada832137570cc8f43c2564728e
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-03 15:50:19 -0400

    Update Spawner.lua

--- DoorsStuffOther/Spawner.lua
+++ DoorsStuffOther/Spawner.lua
@@ -1,3 +1,4 @@
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Spawner Initiated!",true)
 game:GetService("TextChatService").MessageReceived:Connect(function(msg)
 if msg.Text == "/A15" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/A15.lua"))()
