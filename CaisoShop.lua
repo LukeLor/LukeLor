1e8603747f06bd73ddc83c70e15837dbea8f9a26
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-03 07:15:26 -0400

    Modify caption calls for purchase prompts
    Updated caption function calls to use boolean parameter for thought.

--- CaisoShop.lua
+++ CaisoShop.lua
@@ -68,13 +68,13 @@ Shop.PrimaryPart.CanCollide = false
   local chainPrompt = Shop.Shop.IsolatedChainPurchase.Body.PurchasePrompt
  chainPrompt.Triggered:Connect(function()
 --Haha, just caption that you cant pick up. (For now.)
-			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this item, due to the fact I don't even have the correct currency...","thought")
+			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this item, due to the fact I don't even have the correct currency...",true)--thought
     end)
 	local gumdropPrompt = Shop.Shop.Gumdrop.Root.Outer.PurchasePrompt
 	gumdropPrompt.Parent.Parent.Anchored = true
  gumdropPrompt.Triggered:Connect(function()
 --Haha, just caption that you cant pick up. (For now.)
-			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this food, supported by the fact I don't even have the correct currency...","thought")
+			require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can't purchase this food, supported by the fact I don't even have the correct currency...",true)--thought		
 		end)
 	game.Players.LocalPlayer.Character:PivotTo(Shop.PrimaryPart.CFrame)
 	
