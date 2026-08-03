879d83405c241dd7979b9e6a476425d178394432
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-03 16:18:32 -0400

    Change lightCup color to red on entity spawn

--- DoorsStuffOther/A60.lua
+++ DoorsStuffOther/A60.lua
@@ -64,8 +64,8 @@ entity:SetCallback("OnSpawned", function()
     print("Entity has spawned")
 		for _, lightCup in workspace.CurrentRooms:GetDescendants() do
 if lightCup.Parent.Name == "LightFixture" and lightCup.Name == "Neon" then
-lightCup.Color = Color3.fromRGB(83, 156, 82)
-				lightCup.Parent:FindFirstChildOfClass("PointLight").Color = Color3.fromRGB(83, 156, 82)
+lightCup.Color = Color3.fromRGB(255,0,0)
+				lightCup.Parent:FindFirstChildOfClass("PointLight").Color = Color3.fromRGB(255,0,0)
 			end
 		end
 	--[[local entityModel =	entity.Model
