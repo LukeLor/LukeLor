49fbfa2ac22d4e9ac6063e1a02764ce17c0dafdb
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-03 19:27:09 -0400

    Add RushEvery30Main.lua for timed game logic

---
+++ DoorsStuffOther/RushEvery30Main.lua
@@ -0,0 +1,11 @@
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/RushEvery30.lua"))()
+
+
+while task.wait(30) do
+
+  if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 100 and game.ReplicatedStorage.GameData.ChaseInSession.Value == false then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/RushEvery30.lua"))()
+  end
+
+end
