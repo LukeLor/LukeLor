e028c206d07e7a112a67a8a2346c5179c53f699e
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-03 23:02:55 -0400

    Create SilenceEvery30Main.lua

---
+++ DoorsStuffOther/SilenceEvery30Main.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/SilenceEvery30.lua"))()
+
+
+while task.wait(30) do
+
+  if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 100 and game.ReplicatedStorage.GameData.ChaseInSession.Value == false then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/SilenceEvery30.lua"))()
+  end
+
+end
