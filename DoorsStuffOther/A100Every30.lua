5418682fd213aacbb9f39e5c38f0bff20debf761
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-04 19:09:11 -0400

    Add A100Every30.lua to periodically load A100.lua
    This script loads another script every 30 seconds unless certain conditions are met.

---
+++ DoorsStuffOther/A100Every30.lua
@@ -0,0 +1,12 @@
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/A100.lua"))()
+
+
+while task.wait(30) do
+
+  if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 100 and game.ReplicatedStorage.GameData.ChaseInSession.Value == false then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/A100.lua"))()
+  end
+
+end
