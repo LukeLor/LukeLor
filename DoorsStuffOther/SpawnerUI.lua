18b3b37909f9217632545bc8de654f505a3484d9
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-05 15:53:31 -0400

    Update SpawnerUI.lua

--- DoorsStuffOther/SpawnerUI.lua
+++ DoorsStuffOther/SpawnerUI.lua
@@ -5,11 +5,11 @@ local UI = LoadCustomInstance("https://github.com/LukeLor/LukeLor/blob/main/Door
 
 UI.Parent = game.Players.LocalPlayer.PlayerGui
 
-local SetupButton = function(Text, link)
+local SetupButton = function(Text, link, menu) -- Spawn, Timed
 
 local templateButton = UI.MainFrame.SpawnMenu.Template:Clone()
 
-templateButton.Parent = UI.MainFrame.SpawnMenu
+templateButton.Parent = UI.MainFrame[tostring(menu).."Menu"]
 templateButton.Text = tostring(Text)
 templateButton.Name = tostring(Text)
 templateButton.MouseButton1Click:Connect(function()
@@ -19,3 +19,7 @@ end)
 
   
 end
+
+
+
+
