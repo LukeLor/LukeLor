77ed6e8e4df015626389d22c6de283daeb8e1386
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-05 12:47:08 -0400

    Update RushEvery30Main.lua

--- DoorsStuffOther/RushEvery30Main.lua
+++ DoorsStuffOther/RushEvery30Main.lua
@@ -5,7 +5,15 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/
 while task.wait(30) do
 
   if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 100 and game.ReplicatedStorage.GameData.ChaseInSession.Value == false then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/RushEvery30.lua"))()
+local cS = coroutine.create(function()
+task.wait(30)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Next entity in 30 seconds..",true)
+
+
+
+end)
+    coroutine.resume(cS)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/RushEvery30.lua"))()
   end
 
 end
