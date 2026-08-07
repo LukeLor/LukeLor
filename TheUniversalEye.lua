b5bdbaefae752fa1c58b97b4794c4e83e0951104
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-07 09:03:57 -0400

    Update TheUniversalEye.lua

--- TheUniversalEye.lua
+++ TheUniversalEye.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))();
+
 function VisualizeNodes(room)
   local pfs = game:GetService("PathfindingService")
 local startPosition = room:WaitForChild("RoomEntrance").Position
@@ -46,7 +48,8 @@ end
 local nodes = VisualizeNodes(workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value])
 local cnode = nodes[math.random(1,#nodes)]
 
-local model = game:GetObjects("rbxassetid://136064945135625")[1]
+local model = LoadCustomInstance("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/TUE.rbxm")
+
 if not model then return end
 print("Model has loaded!")
 
@@ -55,3 +58,83 @@ if not model.PrimaryPart then return end
 print("PrimaryPart found.")
 model.Parent = workspace
 model:PivotTo(cnode.CFrame)
+
+--MAIN MECHANIC.
+local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")
+local open = false
+local maxDist = 60
+local cDist = 0
+
+for _, light in model:GetDescendants() do
+if light:IsA("PointLight") then
+light:SetAttribute("Brightness", light.Brightness)
+	end
+end
+	
+local functionC = coroutine.create(function()
+	while task.wait() do
+		--print(cDist)
+		cDist = (model.PrimaryPart.Position - char:WaitForChild("HumanoidRootPart").Position).Magnitude
+		if open == true then
+			model.Root.Attachment.Close.Enabled = false
+			model.Root.Attachment.Open.Enabled = true
+			model.Root.Attachment.SparkOpen.Enabled = true
+			model.Root.Attachment.SparkClosed.Enabled = false
+		else
+			model.Root.Attachment.Close.Enabled = true
+			model.Root.Attachment.Open.Enabled = false
+			model.Root.Attachment.SparkOpen.Enabled = false
+			model.Root.Attachment.SparkClosed.Enabled = true
+			model.Root.Attachment.Attack.Enabled = false
+		end
+		if open == true and cDist <= maxDist then
+			local origin = model:GetPivot().Position
+			local charOrigin = char:WaitForChild("HumanoidRootPart").Position
+
+			-- In Sight detection
+			local inSight = false
+			local CollectionService = game:GetService("CollectionService")
+				local params = RaycastParams.new()
+				params.FilterType = Enum.RaycastFilterType.Exclude
+				params.FilterDescendantsInstances = {char, model, CollectionService:GetTagged("HidingSpot")}
+
+				inSight = workspace:Raycast(origin, charOrigin - origin, params) == nil
+			--print(inSight)
+			if inSight then
+				--if char:WaitForChild("Humanoid")
+				if char.Humanoid.MoveDirection.Magnitude > 0 then
+				char.Humanoid.Health -= 0.4
+				
+					model.Root.Attachment.Attack.Enabled = true
+					
+					if model.Root.Attack.Playing then
+						--Nothing
+					else
+						model.Root.Attack:Play()
+					end				
+			else
+				model.Root.Attachment.Attack.Enabled = false
+				end
+				end
+		end
+	end
+end)
+coroutine.resume(functionC)
+
+
+
+local cToggle = coroutine.create(function()
+	while task.wait(math.random(3.5,7)) do
+		open = not open
+		for _, light in model:GetDescendants() do
+if light:IsA("PointLight") then
+game:GetService("TweenService"):Create(light, TweenInfo.new(math.random(5,12)/10), {Brightness = open and light:GetAttribute("Brightness") or 0.5}):Play()
+				end
+			end
+	end
+end)
+task.wait(math.random(2,5)
+coroutine.resume(cToggle)
+
+
