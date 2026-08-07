69472e58d7988a5a71ea66fd7574d01bd99fecb9
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-07 16:13:27 -0400

    Allow jumping and update currency on balance check

--- TestCanJumpBuy.lua
+++ TestCanJumpBuy.lua
@@ -3,5 +3,7 @@ local balance = CurrencyManager:CheckBalance("Shinies")
 if balance >= 30 then
 if game.Players.LocalPlayer.Character then
         game.Players.LocalPlayer.Character:SetAttribute("CanJump", true)
+                CurrencyManager:UpdateCurrency({Identifier = "Shinies"}, -10)
+                require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(tostring(CurrencyManager:CheckBalance("Shinies")).." is your new 'Shinies' balance.", true)
     end
 end
