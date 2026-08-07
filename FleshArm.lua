8cf65df29b935d4a43d3dc304d58688cfaa01b37
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-07 13:51:42 -0400

    Add comment for FleshArm functionality

--- FleshArm.lua
+++ FleshArm.lua
@@ -2,3 +2,5 @@
 --Get model called "Window" search for "LightPanel", then PivotTo() it, then do Stomper mech but locally pivot down Y axis for 20 studs
 
 -- Handle from there
+
+--FleshArm.Hand.PlayerTP
