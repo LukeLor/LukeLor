a67e10f8b4eaf23964172af45e12f60f91c84fed
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-08 14:47:17 -0400

    Update RipperEvery30Main.lua

--- DoorsStuffOther/RipperEvery30Main.lua
+++ DoorsStuffOther/RipperEvery30Main.lua
@@ -5,7 +5,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/
 while task.wait(30) do
 
   if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 100 and game.ReplicatedStorage.GameData.ChaseInSession.Value == false then
-local cS = coroutine.create(function()
+if workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value]:GetAttribute("RawName") ~= "HaltHallway" then
+
+    local cS = coroutine.create(function()
 task.wait(30)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Next entity in 30 seconds..",true)
 
@@ -16,5 +18,5 @@ end)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsStuffOther/RipperEvery30.lua"))()
 
   end
-
+  end
 end
