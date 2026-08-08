6a6e21c666c32e1e8395fc9acc9350284ca85dbe
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-07 21:46:38 -0400

    Update Dasher.lua

--- Dasher.lua
+++ Dasher.lua
@@ -65,19 +65,7 @@ entity:SetCallback("OnSpawned", function()
 end)
 
 entity:SetCallback("OnCrucified", function(player)
-	if player == game.Players.LocalPlayer then
-local CustomAchievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/DOORS-Custom-Achievements/main/init.luau"))()
- CustomAchievements:Grant({
-    Identifier = "OutOfOrderCruc_Dasher",
-    Title = "Undash",
-    Desc = "Seems like someone's got some issues dashing around!",
-    Reason = "Banish Dasher.",
-    Image = "rbxassetid://118705619437445"
-}, {
-    CheckOwned = false,
-    Remember = false
-})
-	end
+	
 end)
 
 entity:SetCallback("OnStartMoving", function()
