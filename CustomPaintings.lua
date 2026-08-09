3cae850c7b38379b4077391641dc54393b73abb1
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-09 15:09:50 -0400

    Enhance painting data structure with new fields
    Added PaintingType and CustomMessage fields to paintings.

--- CustomPaintings.lua
+++ CustomPaintings.lua
@@ -1,14 +1,45 @@
+local MainUI = Player.PlayerGui.MainUI.MainFrame
+
+local PaintingNames = {"Painting_Small", "Painting_Big", "Painting_VeryBig", "Painting_Tall"}
+
 local Possible_Paintings = {
   Descending = {
       ImageId = 135131256657473
-      Name = "Descending"
+      Name = "Descending",
+     PaintingType = "Painting_Big",
+    CustomMessage = nil
 },
   StormySky = {
       ImageId = 119903654385404
-      Name = "Stormy Sky"
+      Name = "Stormy Sky",
+      PaintingType = "Painting_Tall",
+      CustomMessage = nil
 },
   TheFourOfUs = {
       ImageId = 128088934961210
-      Name = "The Four of Us"
+      Name = "The Four of Us",
+      PaintingType = "Painting_VeryBig",
+      CustomMessage = nil
   }
 }
+
+
+function LoadPainting(PaintingTable, PaintingModel)
+  
+    if PaintingModel:GetAttribute("IsCustom") then return end
+    PaintingModel:SetAttribute("IsCustom", true)
+    local ImageLabel = PaintingModel.Canvas.SurfaceGui.ImageLabel
+    ImageLabel.Image = PaintingTable.PaintingImage
+    local Prompt = PaintingModel:FindFirstChildOfClass("ProximityPrompt")
+    local NewPrompt = Prompt:Clone()
+    NewPrompt.Parent = PaintingModel
+  local captionText = PaintingTable.CustomMessage ~= nil and PaintingTable.CustomMessage or "This painting is titled \""..PaintingTable.Name..".\""
+    NewPrompt.Triggered:Connect(function()
+ require(MainUI.Main_Game).caption(captionText, true)
+    end)
+    Prompt:Destroy()
+end
+
+
+
+
