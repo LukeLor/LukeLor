9fb97f0ed60f7b2f64d600135483c022ac8dff41
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-08 23:21:32 -0400

    Add Possible_Paintings table with painting data

---
+++ CustomPaintings.lua
@@ -0,0 +1,14 @@
+local Possible_Paintings = {
+  Descending = {
+      ImageId = 135131256657473
+      Name = "Descending"
+},
+  StormySky = {
+      ImageId = 119903654385404
+      Name = "Stormy Sky"
+},
+  TheFourOfUs = {
+      ImageId = 128088934961210
+      Name = "The Four of Us"
+  }
+}
