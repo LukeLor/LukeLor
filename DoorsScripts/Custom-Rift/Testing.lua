c50f30d277b0aab9b4f030dcdb4b06046968ba0b
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-09 04:12:01 -0400

    Setup TestingRift in Testing.lua

--- DoorsScripts/Custom-Rift/Testing.lua
+++ DoorsScripts/Custom-Rift/Testing.lua
@@ -1,3 +1,6 @@
 local RiftHandler = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsScripts/Custom-Rift/Main.luau"))()
 
 
+RiftHandler:SetupRift({
+Identifier = "TestingRift"
+})
