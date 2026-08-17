a237d25ee2c6377e4eff4d7123382802caf2c785
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-17 19:38:17 -0400

    Fixed URL
    Accidental equal sign :P

--- TPOTRiftSpawn.lua
+++ TPOTRiftSpawn.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/refs/heads/main/Functions.lua"))()
-local Rift = LoadCustomInstance("https://github.com/LukeLor/LukeLor/blob/main/TPOT-Rift.rbxm?=raw=true")
+local Rift = LoadCustomInstance("https://github.com/LukeLor/LukeLor/blob/main/TPOT-Rift.rbxm?raw=true")
 
 Rift.Parent = workspace
 Rift:PivotTo(game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame + Vector3.new(0,13,0))
