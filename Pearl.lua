4f5ed08e824ac6ae9d5590d452be4166eca0ba46
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-20 15:57:45 -0400

    Update Pearl.lua

--- Pearl.lua
+++ Pearl.lua
@@ -23,9 +23,38 @@ local Core = Part:Clone()
 Core.Size = Vector3.new(0.5,0.5,0.5)
 Core.Parent = groupingModel 
 Core.Color = Color3.fromRGB(255,255,255)
-Core.Shape = Enum.PartType.Ball 
 Core.Transparency = 0
 Core.Material = Enum.Material.Neon
+
+local LightAtt1 = Instance.new("Attachment")
+LightAtt1.Parent = Core
+LightAtt1.WorldPosition = Vector3.new(0,0,1)
+
+local LightAtt2 = Instance.new("Attachment")
+LightAtt2.Parent = Core
+LightAtt2.WorldPosition = Vector3.new(0,0,-1)
+
+local LightAtt3 = Instance.new("Attachment")
+LightAtt3.Parent = Core
+LightAtt3.WorldPosition = Vector3.new(0,1,0)
+
+local Light1 = Instance.new("PointLight")
+Light1.Parent = LightAtt1
+Light1.Shadows = true
+Light1.Brightness =1
+Light1.Color = Color3.fromRGB(255, 160, 112)
+
+local Light2 = Instance.new("PointLight")
+Light2.Parent = LightAtt2
+Light2.Shadows = true
+Light2.Brightness =1
+Light2.Color = Color3.fromRGB(158, 215, 255)
+
+local Light3 = Instance.new("PointLight")
+Light3.Parent = LightAtt3
+Light3.Shadows = true
+Light3.Brightness =1
+Light3.Color = Color3.fromRGB(115, 255, 152)
  
 local SpinC = coroutine.create(function()
     while task.wait() do
