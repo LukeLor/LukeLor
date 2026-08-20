f28d6a247dad527ecda951d4738637e6fa804909
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-08-20 17:32:09 -0400

    Enhance Chemeleon behavior with Crucifix checks

--- DoorsStuffOther/Chemeleon.lua
+++ DoorsStuffOther/Chemeleon.lua
@@ -229,9 +229,14 @@ local main = coroutine.create(function()
 if game.ReplicatedStorage.GameData.LatestRoom.Value ~= InitRoomNum then
 canDespawn = true
 			end
-if game.Players.LocalPlayer.Character:GetAttribute("Hiding") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
-				if game.Players.LocalPlayer.Character:FindFirstChild("Crucifix") then
+			if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - model.PrimaryPart.Position).Magnitude < 15 then
+if game.Players.LocalPlayer.Character:FindFirstChild("Crucifix") then
 CrucifixEntity(model, game.Players.LocalPlayer.Character.Crucifix)
+				end
+			end
+				if game.Players.LocalPlayer.Character:GetAttribute("Hiding") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
+				if game.Players.LocalPlayer.Backpack:FindFirstChild("Crucifix") then
+CrucifixEntity(model, game.Players.LocalPlayer.Backpack.Crucifix)
 				else
 					game.Players.LocalPlayer.Character.Humanoid.Health = 0
 				firesignal(game.ReplicatedStorage:WaitForChild("RemotesFolder").DeathHint.OnClientEvent, {"You died to a chemeleon","It hides in the corner of the room and flickers the lights","It hates hiding spots","Dont hide!"}, "Blue")
