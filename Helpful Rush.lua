890ec3b36605c0cddb82ccb97f5ce4240497782c
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-04 18:09:02 -0400

    Update Helpful Rush.lua

--- Helpful Rush.lua
+++ Helpful Rush.lua
@@ -265,7 +265,8 @@ parttofix:Destroy()
 	{"Surge is descending!", "Keep your head up.", "Surge is near.","We're going to get fried if you don't hide!","The sky's yellow..."},
 {"Scribbles is on it's way!", "You know, things are getting shaken up. Maybe Scribbles is near?", "I see paper all over. I think you know what that means.", "Limitless Paper..."}
 , {"Drones incoming!", "Hide, HIde, HIDE!!!", "Get out of their way!", "Drones on break.", "Drones passing through!"},
-	{"Okay, okay, don't look, alright?", "Alma's here...", "Don't- look, Alma...}
+	{"Okay, okay, don't look, alright?", "Alma's here...", "Don't- look, Alma..."},
+	{"Bash-ing through the snow...", "Bash is queued up!", "Bash is here, beware.", "I think you should hide..? Bash is probably on its way."}
 ,
 }
 
@@ -408,6 +409,23 @@ local text = entitytablelines[14][math.random(1,3)]
 		end
 			if inst.Name == "Alma" then
 				local text = entitytablelines[19][math.random(1,3)]
+		print(text)
+		SpeakerIconShow("99087926706059")
+		for i = 1, #text do
+
+
+			wait(0.002)
+			rushhelper.Humanoid:LoadAnimation(rushhelper.Talk):Play()
+			rushhelper.Main.Talk:Play()
+			print(string.sub(text, 1, i))
+
+			Caption(string.sub(text, 1, i))
+			wait(0.002)
+		end
+		SpeakerIconHide()
+			end
+			if inst.Name == "BashMoving" then 
+				local text = entitytablelines[20][math.random(1,4)]
 		print(text)
 		SpeakerIconShow("99087926706059")
 		for i = 1, #text do
