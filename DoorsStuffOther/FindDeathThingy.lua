9e6aa370876981a1909e1e1842393be824689d86
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-06 00:30:35 -0400

    Track registered TextLabels to avoid duplicates

--- DoorsStuffOther/FindDeathThingy.lua
+++ DoorsStuffOther/FindDeathThingy.lua
@@ -1,11 +1,12 @@
 while task.wait() do
-
+local registered = {}
   for _, text in game.Players.LocalPlayer.PlayerGui.MainUI:GetDescendants() do
-if text:IsA("TextLabel") then
+if text:IsA("TextLabel") and table.find(registered, text.Name) then
 local s = text.Text
 local pattern = "Blitz"
 local start_index, end_index = string.find(s, pattern)
       if start_index and end_index then
+       table.insert(registered, text.Name)
 print(text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Paren.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Name..">"..text.Parent.Name..">"..text.Name)
       end
     end
