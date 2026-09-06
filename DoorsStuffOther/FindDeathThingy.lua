2dbddf1821b6fb62a39c4d6b6e73ea8cc43e698f
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-06 00:32:54 -0400

    Fix table insert syntax for registered items

--- DoorsStuffOther/FindDeathThingy.lua
+++ DoorsStuffOther/FindDeathThingy.lua
@@ -6,7 +6,7 @@ local s = text.Text
 local pattern = "Blitz"
 local start_index, end_index = string.find(s, pattern)
       if start_index and end_index then
-       table.insert(registered, text.Name)
+       table.insert(registered, #registered+1, text.Name)
 print(text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Paren.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Parent.Name..">"..text.Parent.Parent.Name..">"..text.Parent.Name..">"..text.Name)
       end
     end
