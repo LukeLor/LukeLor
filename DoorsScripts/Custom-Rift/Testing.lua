2b8c26a816c9312c59cd0cd9fe4233c2873873fa
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-05 21:42:57 -0400

    Erase TestingRift before setup

--- DoorsScripts/Custom-Rift/Testing.lua
+++ DoorsScripts/Custom-Rift/Testing.lua
@@ -1,5 +1,6 @@
 local RiftHandler = loadstring(game:HttpGet("https://raw.githubusercontent.com/LukeLor/LukeLor/refs/heads/main/DoorsScripts/Custom-Rift/Main.luau"))()
 
+RiftHandler:EraseRift("TestingRift")
 
 RiftHandler:SetupRift({
 Identifier = "TestingRift"
