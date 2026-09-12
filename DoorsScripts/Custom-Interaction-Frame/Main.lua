dead64facc1068317fcddf9dcdb57277b4c50a28
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-12 13:04:52 -0400

    Add initial Main.lua for Custom Interaction Frame
    Initial setup for interaction and item UI handling.

---
+++ DoorsScripts/Custom-Interaction-Frame/Main.lua
@@ -0,0 +1,11 @@
+-- Haven't coded yet :P
+
+local Module = {}
+
+Module.SetUp = function(Ftype)
+if string.lower(Ftype) == "interaction" then
+ --Main code here, change workspace Ui prompt, and Ui prompt
+  elseif string.lower(Ftype) == "item"
+    --only change UI item trigger icon
+  end
+end
