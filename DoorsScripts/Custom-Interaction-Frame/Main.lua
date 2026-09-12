7c352a9136135c733a965fa57f5664e332014f54
LJLisCool <155396333+LukeLor@users.noreply.github.com>
2026-09-12 19:17:57 -0400

    Modify SetUp function to include AssetId parameter
    Added AssetId parameter to SetUp function for better functionality.

--- DoorsScripts/Custom-Interaction-Frame/Main.lua
+++ DoorsScripts/Custom-Interaction-Frame/Main.lua
@@ -1,11 +1,13 @@
--- Haven't coded yet :P
+-- Currently gonna try obtaining the stuff necessary 
 
 local Module = {}
 
-Module.SetUp = function(Ftype)
+Module.SetUp = function(Ftype, AssetId)
 if string.lower(Ftype) == "interaction" then
  --Main code here, change workspace Ui prompt, and Ui prompt
   elseif string.lower(Ftype) == "item"
     --only change UI item trigger icon
   end
 end
+
+return Module
